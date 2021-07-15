102e951b146e28f4a4a6a73933edcfc8af1efed8
NoelPAPPI <schlagmullernoel@gmail.com>
2021-07-15 19:00:01 +0200

    NoelPAPPI bot
    Procfile added
    
    Procfile edited
    
    Update bot.js

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODY1MjI0NjE0NTM5ODIxMDk3.YPA5Nw.Dm7vbrRESfMICt9y1Yk6QZ5fUbE',
+        prefix: '_',
+        activity: 'Készítette: NoelPAPPI',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
